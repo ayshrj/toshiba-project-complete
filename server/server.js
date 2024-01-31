b8836fe0e54a5e1d7e4d4377ebd91df9b43532f8
Ayush <ayshrj10@gmail.com>
2024-01-31 16:50:10 +0530

    backend integration

--- server/server.js
+++ server/server.js
@@ -123,5 +123,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running`);
 });
